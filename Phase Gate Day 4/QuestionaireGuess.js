fc39c71a853435ea87277827cea3c59a9e707548
ABEEGOLD <abeeutee@gmail.com>
2025-06-05 16:46:05 +0100

    Phase Gate Day 4

---
+++ Phase Gate Day 4/QuestionaireGuess.js
@@ -0,0 +1,71 @@
+const prompt = require("prompt-sync")();
+
+const Question = []
+const Answer = []
+
+let div = Math.floor(Math.random() * 10);
+ div = {Question: "What is a noun?",Answer: "C", {Option: "A" fool, "B" surplus, "C" a person animal place thing, "D" cat}}
+	{Question: "What is a verb?",Answer: "B", {Option: "A" cat, "B" action, "C" a person, "D" cow}}
+	 {Question: "What is a flower?",Answer: "A", {Option: "A" nice scent, "B" surplus, "C" thing, "D" dog}}
+	 {Question: "What is an adverb?",Answer: "D", {Option: "A" fool, "B" plus, "C" animal , "D" action add}};
+	 {Question: "What is a scale?",Answer: "C", {Option: "A" fool, "B" surplus, "c" length width, "D" cat}};
+	  {Question: "What is a photosynthesis?",Answer: "A", {Option: "A" sun light, "B" sur, "C" place thing, "D" bug}};
+	   {Question: "Who is the president of Nigeria?",Answer: "B", {Option: "A" fil, "B" tinubu, "C" scar, "D" cart}};
+	    {Question: "Who is a citizen?",Answer: "D", {Option: "A" fbl, "B" lus, "C"  place , "D" birth}}
+	    {Question: "Who is the head of the family?",Answer: "B", {Option: "A" kit, "B" father, "C" girl, "D" mother}};
+	     {Question: "What skill did you acquired?",Answer: "D", {Option: "A" facilitate, "B" grak, "C" fishing, "D" tech}};
+
+
+	
+
+	let attemptCounter = 0;
+		while(let attempt < 10){
+			let userInput = prompt("Enter number from 1 to 10:")
+				if(userInput === question[0][0] and userInput === Answer"C" = div){
+					console.log(div("Correct")
+					}else{
+						console.log("Missed")
+					}
+					
+				if(userInput === question[1][1] and userInput === Answer"B" = div){
+					console.log(div("Correct")
+					}else{
+						console.log("Missed")
+					}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
